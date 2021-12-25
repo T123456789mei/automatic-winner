04f737ae232996a90bbef9682568c90e2867ff91
T123456789mei <96484969+T123456789mei@users.noreply.github.com>
2021-12-26 01:12:29 +0800

    嘻嘻
    Signed-off-by: T123456789mei <96484969+T123456789mei@users.noreply.github.com>

--- ceshi/src/hest/csl.java
+++
@@ -1,9 +0,0 @@
-package hest;
-
-public class csl {
-    public static void main(String[] args) {
-        System.out.println( "Hello World");
-        System.out.println( "Hello World");
-
-    }
-}
---
+++ ceshi/src/main/java/hest/csl.java
@@ -0,0 +1,7 @@
+package hest;
+
+public class csl {
+    public static void main(String[] args) {
+        System.out.println( "设置成功");
+    }
+}
